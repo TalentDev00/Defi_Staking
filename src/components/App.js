19d4dc4eb9e55165017bab73f84a800c8ce1442e
TalentDev00 <talentdev0901@gmail.com>
2022-03-27 23:07:18 -0700

    web3 initialization

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,34 @@
 import React, { Component } from 'react'
+import Web3 from 'web3'
 import Navbar from './Navbar'
 import './App.css'
 
 class App extends Component {
+
+	async componentWillUnmount() {
+		await this.loadWeb3()
+		await this.loadBlockchainData()
+	}
+
+	async loadBlockchainData() {
+		const web3 = window.web3
+		
+		const accounts = await web3.eth.getAccounts()
+		console.log(accounts)
+	}
+
+	async loadWeb3()  {
+		if (window.ethereum) {
+			window.web3 = new Web3(window.ethereum)
+			await window.ethereum.enable()
+		}
+		else if (window.web3) {
+			window.web3 = new Web3(window.web3.currentProvider)
+		}
+		else {
+			window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+		}
+	}
 
   constructor(props) {
     super(props)
